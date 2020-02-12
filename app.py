88fe48129a279c3fd092ce8f619797856ac96782
Julian Buet <julian.buet@dafiti.com.ar>
2020-02-11 21:50:39 -0300

    clean code pod

--- app.py
+++ app.py
@@ -17,14 +17,6 @@ while True:
     for i in ret.items:
         for e in i.status.conditions:
             if e.status == 'False':
-                print("Deployment %s at namespace %s have failed. Reason: %s" % (i.metadata.name,i.metadata.namespace e.reason), flush=True)
-                label_selector = ', '.join("{!s}={!r}".format(key,val) for (key,val) in i.spec.selector.match_labels.items())
-                label_selector = label_selector.replace("'","")
-                api_instance = client.CoreV1Api(client.ApiClient())
-                namespace = i.metadata.namespace
-                try:
-                    api_response = api_instance.list_namespaced_pod(namespace, pretty=True, label_selector=label_selector)
-                    #pprint(api_response)
-                except ApiException as e:
-                    print("Exception when calling CoreV1Api->list_namespaced_pod: %s\n" % e, flush=True) 
+                print("Deployment %s at namespace %s have failed. Reason: %s" % (i.metadata.name,i.metadata.namespace, e.reason), flush=True)
+
     time.sleep(30)
